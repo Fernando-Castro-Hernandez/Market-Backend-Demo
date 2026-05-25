20da8e4a1c0c357adc7317d80959a91ec904313b
Fernando-Castro-Hernandez <jesus.castro@tecdesoftware.edu.mx>
2026-05-25 08:56:53 -0600

    feat(persistence): add Getters and Setters from ComprasProductos

--- src/main/java/mx/edu/tecdesotware/Market_Backend_Demo/persistence/entity/ComprasProductos.java
+++ src/main/java/mx/edu/tecdesotware/Market_Backend_Demo/persistence/entity/ComprasProductos.java
@@ -15,9 +15,35 @@ public class ComprasProductos {
     private Boolean estado;
 
 
-    
+    public CompraProductoPK getId() {
+        return id;
+    }
 
+    public void setId(CompraProductoPK id) {
+        this.id = id;
+    }
 
+    public Integer getCantidad() {
+        return cantidad;
+    }
 
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
 
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
 }
